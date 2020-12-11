118f7cd17e189488b3a79e370ae1f83759513dd1
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-12-11 11:15:50 -0700

    Add items table and data to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-require 'csv'
+# require 'csv'
 
-cmd = "pg_restore --verbose --clean --no-acl --no-owner -h localhost -U $(whoami) -d rails_engine_development db/data/rails-engine-development.pgdump"
-puts "Loading PostgreSQL Data dump into local database with command:"
-puts cmd
-system(cmd)
+# cmd = "pg_restore --verbose --clean --no-acl --no-owner -h localhost -U $(whoami) -d rails_engine_development db/data/rails-engine-development.pgdump"
+# puts "Loading PostgreSQL Data dump into local database with command:"
+# puts cmd
+# system(cmd)
+
+# filename = "db/data/item.csv"
+#
+# csv = CSV.foreach(Rails.root.join(filename), :headers => true) do |row|
+#   row["unit_price"] = (row["unit_price"].to_f)/100.round(2)
+#   Item.create(row.to_hash)
+# end
