fc73381b0907547176d82f511228814cb93f9527
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-12-11 12:47:08 -0700

    Add customer and invoice relationships and tests

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe Merchant, type: :model do
+RSpec.describe Customer, type: :model do
   describe "relationships" do
-    it { should have_many :items }
     it { should have_many :invoices }
   end
 end
