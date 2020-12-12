bd4ddd34c9df4eceb6f14527b3b43174448b5746
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-12-12 14:26:25 -0700

    Add test to create new merchant

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -30,4 +30,18 @@ describe "Merchants API" do
     expect(merchant).to have_key(:name)
     expect(merchant[:name]).to be_a(String)
   end
+
+  it "can create a new merchant" do
+    merchant_params = ({
+      name: "Bill's Barbour Shop"
+      })
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post '/api/v1/merchants', headers: headers, params: JSON.generate(merchant: merchant_params)
+    created_merchant = Merchant.last
+
+    expect(response).to be_successful
+
+    expect(created_merchant.name).to eq(merchant_params[:name])
+  end
 end
