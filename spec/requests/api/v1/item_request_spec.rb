dd4e369d29f64876a6ca46a6bc83f531382e7b10
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-12-14 15:14:28 -0700

    Add test for item show

--- spec/requests/api/v1/item_request_spec.rb
+++ spec/requests/api/v1/item_request_spec.rb
@@ -46,4 +46,44 @@ describe "Items API" do
     end
     DatabaseCleaner.clean
   end
+
+  it "sends a list of items", type: :request do
+    DatabaseCleaner.start
+    id = create(:item).id
+
+    get "/api/v1/items/#{id}"
+
+    expect(response).to be_successful
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    item = json[:data]
+
+    expect(item).to have_key(:id)
+    expect(item[:id]).to be_a(String)
+
+    expect(item).to have_key(:type)
+    expect(item[:type]).to be_a(String)
+
+    expect(item).to have_key(:attributes)
+    expect(item[:attributes]).to be_a(Hash)
+
+    item_data = item[:attributes]
+
+    expect(item_data).to have_key(:id)
+    expect(item_data[:id]).to be_a(Integer)
+
+    expect(item_data).to have_key(:name)
+    expect(item_data[:name]).to be_a(String)
+
+    expect(item_data).to have_key(:description)
+    expect(item_data[:description]).to be_a(String)
+
+    expect(item_data).to have_key(:unit_price)
+    expect(item_data[:unit_price]).to be_a(Float)
+
+    expect(item_data).to have_key(:merchant_id)
+    expect(item_data[:merchant_id]).to be_an(Integer)
+
+    DatabaseCleaner.clean
+  end
 end
