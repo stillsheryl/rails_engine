d616f1d3da5b2aae6c6885677166f00e1db326a6
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-12-14 17:08:56 -0700

    Add test that items can be deleted

--- spec/requests/api/v1/item_request_spec.rb
+++ spec/requests/api/v1/item_request_spec.rb
@@ -190,4 +190,16 @@ describe "Items API" do
 
     DatabaseCleaner.clean
   end
+
+  it "can delete an existing item", type: :request do
+    item = create(:item)
+
+    expect(Item.count).to eq(1)
+
+    delete "/api/v1/items/#{item.id}"
+
+    expect(response).to be_successful
+    expect(Item.count).to eq(0)
+    expect{Item.find(item.id)}.to raise_error(ActiveRecord::RecordNotFound)
+  end
 end
