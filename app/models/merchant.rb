2fa4ed14095cbb6bc3c5e17939e3ed6146c381c7
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-12-15 20:30:41 -0700

    WIP - merchant revenue, needs refactoring

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,19 @@
 class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
+  has_many :transactions, through: :invoices
+  has_many :invoice_items, through: :invoices
+
+  def total_revenue
+    invoices = self.invoices
+    .select("sum(invoice_items.quantity * invoice_items.unit_price) AS revenue")
+    .joins(:transactions, :invoice_items)
+    .where("invoices.status = 'shipped' AND transactions.result = 'success' AND invoices.merchant_id = '#{self.id}'")
+    .group(:id)
+    .order("revenue DESC")
+
+    invoices.sum do |rev|
+      rev.revenue
+    end
+  end
 end
