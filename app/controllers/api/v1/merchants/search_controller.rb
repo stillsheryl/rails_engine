fb7239418b7ab485c3328da62b742a3c10a05fff
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-12-16 15:10:49 -0700

    Update method for single merchant search to include dates

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -14,9 +14,16 @@ class  Api::V1::Merchants::SearchController < ApplicationController
   end
 
   def show
-    keyword = params[:keyword].downcase
-
-    merchant_result = Merchant.where("LOWER(name) LIKE ?", "%#{keyword}%").limit(1)
-    render json: MerchantSerializer.new(merchant_result)
+    if params["name"] != nil
+      keyword = params["name"].downcase
+      merchant_results = Merchant.find_by("LOWER(name) LIKE ?", "%#{keyword}%")
+    elsif params["created_at"] != nil
+      keyword = params["created_at"].to_datetime
+      merchant_results = Merchant.find_by(:created_at => keyword)
+    elsif params["updated_at"] != nil
+      keyword = params["updated_at"].to_datetime
+      merchant_results = Merchant.find_by(:updated_at => keyword)
+    end
+    render json: MerchantSerializer.new(merchant_results)
   end
 end
