ad121f2e81e60ab77a3278c0a1073365856113ce
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-12-17 14:51:01 -0700

    Delete comments from previous attempts

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -23,29 +23,3 @@ class Merchant < ApplicationRecord
     .limit(limit)
   end
 end
-
-
-# self.invoices.select("sum(invoice_items.quantity * invoice_items.unit_price) AS revenue").joins(:transactions, :invoice_items).where("invoices.merchant_id = '#{self.id}'").merge(Transaction.successful).merge(Invoice.shipped).group(:id).order("revenue DESC")
-
-# self.invoices.select("sum(invoice_items.quantity * invoice_items.unit_price) AS revenue").joins(:transactions, :invoice_items).merge(Transaction.successful).merge(Invoice.shipped).group(:id)
-#
-# SELECT sum(invoice_items.quantity * invoice_items.unit_price) AS revenue
-# FROM "invoices"
-# INNER JOIN "transactions" ON "transactions"."invoice_id" = "invoices"."id"
-# INNER JOIN "invoice_items" ON "invoice_items"."invoice_id" = "invoices"."id"
-# WHERE "invoices"."merchant_id" = 9186 AND "transactions"."result" = 'success' AND "invoices"."status" = 'shipped'
-# GROUP BY "invoices"."merchant_id"
-#
-#
-# # invoices = self.invoices
-# # .select("sum(invoice_items.quantity * invoice_items.unit_price) AS revenue")
-# # .joins(:transactions, :invoice_items)
-# # .where("invoices.merchant_id = '#{self.id}'")
-# # .merge(Transaction.successful)
-# # .merge(Invoice.shipped)
-# # .group(:id)
-# # .order("revenue DESC")
-# #
-# invoices.sum do |rev|
-#   rev.revenue
-# end
