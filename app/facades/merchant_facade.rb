db3732d7e51691c4c654d39edd234f2536293bcb
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-12-18 07:45:15 -0700

    Add merchant with most items sold facade

--- app/facades/merchant_facade.rb
+++ app/facades/merchant_facade.rb
@@ -1,7 +1,14 @@
 class MerchantFacade
   def self.merchants_with_most_revenue(quantity)
-    merchant = Merchant.most_revenue(quantity)
-    merchant.map do |merchant|
+    merchants = Merchant.most_revenue(quantity)
+    merchants.map do |merchant|
+      MerchantObject.new(merchant)
+    end
+  end
+
+  def self.merchants_with_most_items_sold(quantity)
+    merchants = Merchant.most_items_sold(quantity.to_i)
+    merchants.map do |merchant|
       MerchantObject.new(merchant)
     end
   end
