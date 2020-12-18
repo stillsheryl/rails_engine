03a483188ec070a92a120f24ce1f62418ac09ce6
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-12-17 19:54:07 -0700

    Update to user revenue serializer

--- app/controllers/api/v1/revenue_controller.rb
+++ app/controllers/api/v1/revenue_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::RevenueController < ApplicationController
   def show
-    render json: MerchantSerializer.new(Merchant.revenue_by_date(date_params))
+    render json: RevenueSerializer.new(Merchant.revenue_by_date(date_params))
   end
-
+  
   private
 
   def date_params
